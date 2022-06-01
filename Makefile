86ef156cbfb98212ce7b7488a8bc78990ebc7678
MEXAHO3ABP: WorkPC (UOnWin10) <gva_lt@list.ru>
2022-06-01 16:09:31 +0700

    step 4

--- Makefile
+++ Makefile
@@ -5,3 +5,5 @@ gendiff: #Запуск gendiff
 	./bin/gendiff
 lint: #Проверка линтером php-codesniffer по стандарту PSR12
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+test: #Запуск тестов
+	composer exec --verbose phpunit tests
---
+++ tests/gendiffTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Hexlet\Code\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Hexlet\Code\Gendiff\gendiff;
+
+class gendiffTest extends TestCase
+{
+    public function testGendiff(): void
+    {
+    $result1 = "{\n" .
+        "  - follow: false\n" .
+        "    host: hexlet.io\n" .
+        "  - proxy: 123.234.53.22\n" .
+        "  - timeout: 50\n" .
+        "  + timeout: 20\n" .
+        "  + verbose: true\n" . '}';
+    $result2 = "{\n" .
+        "  + abergemaht: false\n" .
+        "  - follow: false\n" .
+        "    host: hexlet.io\n" .
+        "  - proxy: 123.234.53.22\n" .
+        "  + rust: 12\n" .
+        "  - timeout: 50\n" .
+        "  + timeout: 20\n" .
+        "  + verbose: true\n" . '}';
+        $this->assertEquals($result1, gendiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json'));
+        $this->assertEquals($result2, gendiff('./tests/fixtures/file1.json', './tests/fixtures/file3.json'));
+    }
+}
