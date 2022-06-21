cb6f746baa91745c43bd128c7933c5d756d0266c
MEXAHO3ABP: WorkPC (UOnWin10) <gva_lt@list.ru>
2022-06-21 12:26:19 +0700

    final: fix bug with mutable in json.php

--- src/Formatters/json.php
+++ src/Formatters/json.php
@@ -9,6 +9,7 @@ use function Hexlet\Code\MyFunctions\testLastOnArray;
 /**
  * @param array<mixed> $array
  * @param array<mixed> $prevArray
+ * @param array<mixed> $result
  *
  * Функция реализует рекурсивный метод для форматирования вывода диффа типа json:
  * {
@@ -17,7 +18,7 @@ use function Hexlet\Code\MyFunctions\testLastOnArray;
  *   "+ timeout": 20
  * }
  */
-function json(array $array, int $depth = 0, int $flagOnArray = 0, array $prevArray = [], string $result = ''): string
+function json(array $array, int $depth = 0, int $flagOnArray = 0, array $prevArray = [], array $result = []): string
 {
     $abzac = '    ';
 
@@ -27,97 +28,99 @@ function json(array $array, int $depth = 0, int $flagOnArray = 0, array $prevArr
         if (array_key_exists('itsGendiff', $value) && $value['itsGendiff'] === '->yes<-') {
             /** Формируется строка диффа для случаев +, -, =, -array1, -array2 и -+ */
             if (($value['diffType'] === '+' || $value['diffType'] === '-') && $flagOnArray === 0) {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . $value['diffType'];
-                $result .= ' ' . $value['key'] . "\": " . testOnTrueFalseNull($value['value'], "\"");
-                $result .= $value['value'] . testOnTrueFalseNull($value['value'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . $value['diffType'];
+                $result[] = ' ' . $value['key'] . "\": " . testOnTrueFalseNull($value['value'], "\"");
+                $result[] = $value['value'] . testOnTrueFalseNull($value['value'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
             } elseif ($value['diffType'] === '=' && $flagOnArray === 0) {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '~ ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['value'], "\"") . $value['value'];
-                $result .= testOnTrueFalseNull($value['value'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '~ ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['value'], "\"") . $value['value'];
+                $result[] = testOnTrueFalseNull($value['value'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
             /** если дифф лежит внутри массива, который целиком является диффом, то строка диффа не содержит знаков + или - */
             } elseif (($value['diffType'] === '+' || $value['diffType'] === '-') && $flagOnArray === 1) {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '  ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['value'], "\"") . $value['value'];
-                $result .= testOnTrueFalseNull($value['value'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '  ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['value'], "\"") . $value['value'];
+                $result[] = testOnTrueFalseNull($value['value'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
             } elseif ($value['diffType'] === '=' && $flagOnArray === 1) {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '~ ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['value'], "\"") . $value['value'];
-                $result .= testOnTrueFalseNull($value['value'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '~ ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['value'], "\"") . $value['value'];
+                $result[] = testOnTrueFalseNull($value['value'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
             } elseif ($value['diffType'] === '-+') {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '- ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['value'], "\"") . $value['value'];
-                $result .= testOnTrueFalseNull($value['value'], "\"");
-                $result .= ",\n";
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '+ ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['oldValue'], "\"") . $value['oldValue'];
-                $result .= testOnTrueFalseNull($value['oldValue'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '- ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['value'], "\"") . $value['value'];
+                $result[] = testOnTrueFalseNull($value['value'], "\"");
+                $result[] = ",\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '+ ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['oldValue'], "\"") . $value['oldValue'];
+                $result[] = testOnTrueFalseNull($value['oldValue'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
             } elseif ($value['diffType'] === '-array1') {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '- ' . $value['key'] . "\"" . ': {' . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '- ' . $value['key'] . "\"" . ': {' . "\n";
                 foreach ($value['oldValue'] as $key2 => $value2) {
-                    $result .= str_repeat($abzac, $depth + 2) . $abzac . "\"" . '  ' . $key2 . "\": ";
-                    $result .= testOnTrueFalseNull($value2, "\"") . $value2;
-                    $result .= testOnTrueFalseNull($value2, "\"");
-                    $result .= testLastOnArray($value, $value['oldValue'], $key2, ',') . "\n";
+                    $result[] = str_repeat($abzac, $depth + 2) . $abzac . "\"" . '  ' . $key2 . "\": ";
+                    $result[] = testOnTrueFalseNull($value2, "\"") . $value2;
+                    $result[] = testOnTrueFalseNull($value2, "\"");
+                    $result[] = testLastOnArray($value, $value['oldValue'], $key2, ',') . "\n";
                 }
-                $result .= str_repeat($abzac, $depth) . $abzac . '}';
-                $result .= testLastOnArray($prevArray, $value, $value['key'], ',') . "\n";
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '+ ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['value'], "\"") . $value['value'];
-                $result .= testOnTrueFalseNull($value['value'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . '}';
+                $result[] = testLastOnArray($prevArray, $value, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '+ ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['value'], "\"") . $value['value'];
+                $result[] = testOnTrueFalseNull($value['value'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
             } elseif ($value['diffType'] === '-array2') {
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '- ' . $value['key'] . "\": ";
-                $result .= testOnTrueFalseNull($value['value'], "\"") . $value['value'];
-                $result .= testOnTrueFalseNull($value['value'], "\"");
-                $result .= testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
-                $result .= str_repeat($abzac, $depth) . $abzac . "\"" . '+ ' . $value['key'] . "\"" . ': {' . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '- ' . $value['key'] . "\": ";
+                $result[] = testOnTrueFalseNull($value['value'], "\"") . $value['value'];
+                $result[] = testOnTrueFalseNull($value['value'], "\"");
+                $result[] = testLastOnArray($value, $prevArray, $value['key'], ',') . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . "\"" . '+ ' . $value['key'] . "\"" . ': {' . "\n";
                 foreach ($value['oldValue'] as $key3 => $value3) {
-                    $result .= str_repeat($abzac, $depth + 2) . '  ' . "\"" . '  ' . $key3 . "\": ";
-                    $result .= testOnTrueFalseNull($value3, "\"") . $value3;
-                    $result .= testOnTrueFalseNull($value3, "\"");
-                    $result .= testLastOnArray($value, $value['oldValue'], $key3, ',') . "\n";
+                    $result[] = str_repeat($abzac, $depth + 2) . '  ' . "\"" . '  ' . $key3 . "\": ";
+                    $result[] = testOnTrueFalseNull($value3, "\"") . $value3;
+                    $result[] = testOnTrueFalseNull($value3, "\"");
+                    $result[] = testLastOnArray($value, $value['oldValue'], $key3, ',') . "\n";
                 }
-                $result .= str_repeat($abzac, $depth) . $abzac . '},' . "\n";
+                $result[] = str_repeat($abzac, $depth) . $abzac . '},' . "\n";
             }
         } else {
             /** Для каждого массива, не являющегося диффом формируется (увеличивается для каждого вызова функции на 1) */
             /** его глубина, участвующая в формировании отступа слева для каждого вывода диффа, */
             $depth += 1;
-            $result .= str_repeat($abzac, $depth);
-            $result .= "\"";
+            $result[] = str_repeat($abzac, $depth);
+            $result[] = "\"";
             /** формируется знак +, - или ' ' и флаг массива диффов, если он целиком является диффом (все диффы содержат diffType = '+' ), */
             /** а также запоминается уровень с которого нырнули в массив, целиком являющийся диффом */
             if (testOnDiffArray($value, '+') && $flagOnArray === 0) {
-                $result .= '+ ';
+                $result[] = '+ ';
                 $flagOnArray = 1;
                 $depthOnArray = $depth;
             } elseif (testOnDiffArray($value, '-') && $flagOnArray === 0) {
-                $result .= '- ';
+                $result[] = '- ';
                 $flagOnArray = 1;
                 $depthOnArray = $depth;
             } else {
-                $result .= '~ ';
+                $result[] = '~ ';
             }
             /** вписывается ключ, символы ': {' */
-            $result .= $key . "\"" . ': {' . "\n";
+            $result[] = $key . "\"" . ': {' . "\n";
             /** Затем вызывается рекурсивно функция json с передачей ей глубины и флага того, что массив целиком дифф */
-            $result .= json($value, $depth, $flagOnArray, $value);
+            $result[] = json($value, $depth, $flagOnArray, $value);
             /** Если вернулись на уровень откуда нырнули, то снимаем флаг массива-диффа, */
             if (isset($depthOnArray) && $depth === $depthOnArray) {
                 $flagOnArray = 0;
             }
             /** уменьшаем глубину, */
             $depth -= 1;
-            $result .= str_repeat($abzac, $depth) . $abzac . '}';
+            $result[] = str_repeat($abzac, $depth) . $abzac . '}';
             /** завершаем вывод диффа массива закрывающей скобкой */
-            $result .= testLastOnArray($array, $array, $key, ',') . "\n";
+            $result[] = testLastOnArray($array, $array, $key, ',') . "\n";
         }
     }
 
-    return $result;
+    $resString = implode('', $result);
+
+    return $resString;
 }
