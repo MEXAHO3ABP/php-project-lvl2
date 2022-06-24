7129b658880e07522f9d3cded256569f77e0d064
MEXAHO3ABP: WorkPC (UOnWin10) <gva_lt@list.ru>
2022-06-24 13:27:17 +0700

    final: refactory: attempt 2+

--- tests/gendiffTest.php
+++ tests/gendiffTest.php
@@ -13,7 +13,7 @@ class GendiffTest extends TestCase
         $result1 = file_get_contents('tests/fixtures/result.stylish');
         $result2 = file_get_contents('tests/fixtures/result.plain');
         $result3 = file_get_contents('tests/fixtures/result.json');
-        
+
         $this->assertEquals($result1, gendiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'stylish'));
         $this->assertEquals($result2, gendiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'plain'));
         $this->assertEquals($result3, gendiff('./tests/fixtures/file1.json', './tests/fixtures/file2.json', 'json'));
